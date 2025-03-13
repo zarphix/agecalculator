3969e090ebd78add6c0e2d53a95769322251a4ef
zarphix <thomas@familie-de-jong.nl>
2025-03-13 09:59:42 +0000

    everything lmao

---
+++ age.js
@@ -0,0 +1,10 @@
+function calculate_age(dob) {
+    let diff_ms = Date.now() - dob.getTime();
+    let age_dt = new Date(diff_ms); 
+
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+let age = calculate_age(new Date(/* year = */ 1998,/* month = */ 3,/* day = */ 7));
+
+console.log(age);
